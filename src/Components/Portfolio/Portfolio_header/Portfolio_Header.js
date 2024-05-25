0386f0c3b8b778c13de972f36bd19971989c8cc2
Moaz Ahmed <137870752+theCoderMoazAhmed@users.noreply.github.com>
2024-05-25 19:17:35 +0500

    completed the frontend of portfolio page

--- src/Components/Portfolio/Portfolio_header/Portfolio_Header.js
+++ src/Components/Portfolio/Portfolio_header/Portfolio_Header.js
@@ -1,10 +1,46 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React, { useState,useRef,useEffect } from "react";
 import "./Portfolio_Header.css";
 import gsap from "gsap";
 import Link from "next/link";
+import { IoIosArrowDown } from "react-icons/io";
+import { useGSAP } from "@gsap/react";
 
 const Portfolio_Header = () => {
+  const [openCategories, setOpenCategories] = useState(null);
+
+  useGSAP(() => {
+    const headingElement = document.querySelector(".pf_h_heading h1");
+
+    // Apply the GSAP animation to the heading element
+    gsap.fromTo(
+      headingElement,
+      {
+        top: "100%",
+      },
+      {
+        top: "0%",
+        duration: 1,
+        ease: "power3.out",
+      }
+    );
+  });
+
+  const toggleCategories = (index) => {
+    const newIndex = openCategories === index ? null : index;
+    setOpenCategories(newIndex);
+  };
+
+
+
+
+
+
+
+
+
+
+
   const topAnimationRef = useRef(null);
 
   useEffect(() => {
@@ -64,12 +100,80 @@ const Portfolio_Header = () => {
 
   return (
     <div className="portfolio_header">
-      <div className="p_header_info">
-        <h1 className="p_h_heading">Portfolio</h1>
-        <div className="portfolio_nav">
-          <Link href={"/"}>All</Link>
-          <Link href={"/"}>Websites</Link>
-          <Link href={"/"}>Gfsx&nbsp;Design</Link>
+      <div className="pf_h_info">
+        <div className="pf_h_heading">
+          <h1>Portfolio</h1>
+        </div>
+        <div className="pf_links">
+          <div className="pf_link">
+            <Link href={"#"}>All</Link>{" "}
+            <span
+              className="pf_link_categories_show"
+              onClick={() => toggleCategories(0)}
+            >
+              <IoIosArrowDown />
+            </span>
+            <div
+              className={`pf_link_categories ${
+                openCategories === 0 ? "visible" : ""
+              }`}
+            >
+              <Link href={'#'} className="pf_link_category">Gfx&nbsp;Design</Link>
+              <Link href={'#'} className="pf_link_category">Web&nbsp;Developing</Link>
+              <Link href={'#'} className="pf_link_category">Video&nbsp;Editing</Link>
+            </div>
+          </div>
+          <div className="pf_link">
+            <Link href={"#"}>Gfx&nbsp;Design</Link>{" "}
+            <span
+              className="pf_link_categories_show"
+              onClick={() => toggleCategories(1)}
+            >
+              <IoIosArrowDown />
+            </span>
+            <div
+              className={`pf_link_categories ${
+                openCategories === 1 ? "visible" : ""
+              }`}
+            >
+              <Link href={'#'} className="pf_link_category">Logos</Link>
+              <Link href={'#'} className="pf_link_category">Banners</Link>
+            </div>
+          </div>
+          <div className="pf_link">
+            <Link href={"#"}>Video&nbsp;Editing</Link>{" "}
+            <span
+              className="pf_link_categories_show"
+              onClick={() => toggleCategories(2)}
+            >
+              <IoIosArrowDown />
+            </span>
+            <div
+              className={`pf_link_categories ${
+                openCategories === 2 ? "visible" : ""
+              }`}
+            >
+              <Link href={'#'} className="pf_link_category">Motion</Link>
+              <Link href={'#'} className="pf_link_category">Blender</Link>
+            </div>
+          </div>
+          <div className="pf_link">
+            <Link href={"#"}>Web&nbsp;Developing</Link>{" "}
+            <span
+              className="pf_link_categories_show"
+              onClick={() => toggleCategories(3)}
+            >
+              <IoIosArrowDown />
+            </span>
+            <div
+              className={`pf_link_categories ${
+                openCategories === 3 ? "visible" : ""
+              }`}
+            >
+              <Link href={'#'} className="pf_link_category">Websites</Link>
+              <Link href={'#'} className="pf_link_category">Web&nbsp;Apps</Link>
+            </div>
+          </div>
         </div>
       </div>
       <div className="p_header_animation">
